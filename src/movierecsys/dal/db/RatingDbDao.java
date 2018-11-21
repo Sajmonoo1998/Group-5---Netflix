d9070093c8c3c1716102c3a62fb18eff1a740a8d
leopoldus97 <mate1011@easv365.dk>
2018-11-21 13:23:07 +0100

    ratingdbdao without the getRatings()

--- src/movierecsys/dal/db/RatingDbDao.java
+++ src/movierecsys/dal/db/RatingDbDao.java
@@ -9,7 +9,10 @@ import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,25 +36,71 @@ public class RatingDbDao implements IRatingRepository
     @Override
     public void createRating(Rating rating)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try(Connection con = conProvider.getConnection())
+        {
+            String sql = "INSERT INTO Movie(movieId, userId, rating) VALUES(?, ?, ?)";
+            PreparedStatement prtr = con.prepareStatement(sql);
+            prtr.setInt(1, rating.getMovie());
+            prtr.setInt(2, rating.getUser());
+            prtr.setInt(3, rating.getRating());
+            prtr.execute();
+//            Rating r = new Rating(rating.getMovie(), rating.getUser(), rating.getRating());
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void deleteRating(Rating rating)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try(Connection con = conProvider.getConnection())
+        {
+            String sql = "DELETE FROM Ratings WHERE rating = ?";
+            PreparedStatement prtr = con.prepareStatement(sql);
+            prtr.setInt(1, rating.getRating());
+            prtr.execute();
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public List<Rating> getAllRatings() throws IOException
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Rating> ratings = new ArrayList<>();
+        
+        try (Connection con = conProvider.getConnection())
+        {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Movie;");
+            while(rs.next())
+            {
+                int movieId = rs.getInt("movieId");
+                int userId = rs.getInt("userId");
+                int rating = rs.getInt("rating");
+                Rating r = new Rating(movieId, userId, rating);
+                ratings.add(r);
+            }
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+        return ratings;
     }
 
     @Override
     public List<Rating> getRatings(User user)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
 
     @Override
@@ -59,7 +108,7 @@ public class RatingDbDao implements IRatingRepository
     {
         try (Connection con = conProvider.getConnection();)
         {
-            String sql = "UPDATE Rating SET userId = ?, ratings = ? WHERE movieId = ?";
+            String sql = "UPDATE Ratings SET userId = ?, rating = ? WHERE movieId = ?";
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setInt(1, rating.getUser());
             stmt.setInt(2, rating.getRating());
