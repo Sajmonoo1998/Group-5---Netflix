b6baa2beebbfb5dcb4de5553e382b6f9bf7329f8
PLKING <sz.soczewka@gmail.com>
2018-11-21 14:12:44 +0100

    21/11

---
+++ Group-5---Netflix-path1/src/movierecsys/dal/db/DbConnectionProvider.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.dal.db;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author pgn
+ */
+public class DbConnectionProvider
+{
+
+    private SQLServerDataSource ds;
+    private static final String PROP_FILE = "Marek.txt";
+
+    public DbConnectionProvider() throws FileNotFoundException, IOException
+    {
+        Properties databaseProperties = new Properties();
+        databaseProperties.load(new FileInputStream(PROP_FILE));
+        ds = new SQLServerDataSource();
+        ds.setServerName(databaseProperties.getProperty("ServerName"));
+        ds.setDatabaseName(databaseProperties.getProperty("DatabaseName"));
+        ds.setUser(databaseProperties.getProperty("Login"));
+        ds.setPassword(databaseProperties.getProperty("Password"));
+    }
+    
+    public Connection getConnection() throws SQLServerException
+    {
+        return ds.getConnection();
+    }
+
+}
---
+++ Group-5---Netflix-path1/src/movierecsys/dal/db/RatingDbDao.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.dal.db;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import movierecsys.be.Movie;
+import movierecsys.be.Rating;
+import movierecsys.be.User;
+import movierecsys.dal.intereface.IRatingRepository;
+
+/**
+ *
+ * @author pgn
+ */
+public class RatingDbDao implements IRatingRepository
+{
+    private DbConnectionProvider conProvider;
+    
+    public RatingDbDao() throws IOException
+    {
+        conProvider = new DbConnectionProvider();
+    }
+
+    @Override
+    public void createRating(Rating rating)
+    {
+ 
+        try(Connection con = conProvider.getConnection())
+        {
+            String sql = "INSERT INTO Movie(movieId, userId, rating) VALUES(?, ?, ?)";
+            PreparedStatement prtr = con.prepareStatement(sql);
+            prtr.setInt(1, rating.getMovie());
+            prtr.setInt(2, rating.getUser());
+            prtr.setInt(3, rating.getRating());
+            prtr.execute();
+//            Rating r = new Rating(rating.getMovie(), rating.getUser(), rating.getRating());
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ }
+
+    @Override
+    public void deleteRating(Rating rating)
+    {
+        try(Connection con = conProvider.getConnection())
+        {
+            String sql = "DELETE FROM Ratings WHERE rating = ?";
+            PreparedStatement prtr = con.prepareStatement(sql);
+            prtr.setInt(1, rating.getRating());
+            prtr.execute();
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public List<Rating> getAllRatings() throws IOException
+     {
+        List<Rating> ratings = new ArrayList<>();
+        
+        try (Connection con = conProvider.getConnection())
+        {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Movie;");
+            while(rs.next())
+            {
+                int movieId = rs.getInt("movieId");
+                int userId = rs.getInt("userId");
+                int rating = rs.getInt("rating");
+                Rating r = new Rating(movieId, userId, rating);
+                ratings.add(r);
+            }
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+        return ratings;
+    }
+
+    @Override
+    public List<Rating> getRatings(User user)
+    {
+        List<Rating> lr = new ArrayList<>();
+       try (Connection con = conProvider.getConnection();)
+        {
+            PreparedStatement pstmt = con.prepareStatement("SELECT * FROM Rating WHERE UserID = ?");
+            pstmt.setInt(1, user.getId());
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next())
+            {
+                
+                Rating r = new Rating(rs.getInt("MovieID"),user.getId(),rs.getInt("rating"));
+                lr.add(r);
+               
+            }
+             return lr;
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void updateRating(Rating rating) throws IOException
+    {
+        try (Connection con = conProvider.getConnection();)
+        {
+            String sql = "UPDATE Rating SET userId = ?, ratings = ? WHERE movieId = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, rating.getUser());
+            stmt.setInt(2, rating.getRating());
+            stmt.setInt(3, rating.getMovie());
+            stmt.execute();
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(MovieDbDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
